a068bee80cf4368793261efd0079dcbf189a76dc
AlirezaNazari <51047310+ItsAlirezaNazari@users.noreply.github.com>
2022-03-02 18:53:56 +0330

    Add files via upload

---
+++ Assets/Scripts/BallSpeed.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpeed : MonoBehaviour
+{
+    public float speed = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
